b00cafe4803d60ef0fbbb81d492a2aa230fe6c62
UNIkeEN <2641712741@qq.com>
2024-06-28 17:59:09 +0800

    feat: update main.cpp 0628

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
 #include <glad/glad.h>
 #include "viewer/viewer.h"
+#include "viewer/camera_perspective.hpp"
 #include "render/render_OpenGL.h"
 
 int main() {
 
-    std::shared_ptr<Render> renderer = std::make_shared<OpenGLRender>();
+    const int WIDTH = 1920, HEIGHT = 1080;
 
-    Viewer viewer(800, 600, renderer, nullptr);
+    std::shared_ptr<Render> renderer = std::make_shared<OpenGLRender>();
+    std::shared_ptr<Camera> camera = std::make_shared<PerspectiveCamera>(WIDTH/(float)HEIGHT);
+    Viewer viewer(WIDTH, HEIGHT, renderer, camera);
+    Scene scene;
+    scene.addModel("../assets/SJTU_east_gate_MC/East_Gate_Voxel.obj");
+
     viewer.init();
+    renderer->init();
+    renderer->setup(std::make_shared<Scene>(scene));
+
     viewer.mainLoop();
+    
+    renderer->cleanup();
     viewer.cleanup();
+    scene.cleanup();
+
+    // system("pause");
     return 0;
 }
